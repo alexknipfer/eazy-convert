fd99da32849cf4ef1ea5a5d1ca6cb3fb260cd931
Alex Knipfer <cr33pyxjtag@yahoo.com>
2016-10-15 21:37:39 -0500

    project initialized

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import PriceForm from './PriceForm.js';
+
+class App extends Component {
+  render() {
+    return (
+      <PriceForm />
+    );
+  }
+}
+
+export default App;
---
+++ src/PriceForm.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import {Card, CardTitle, CardText} from 'material-ui/Card';
+import './priceForm.css';
+import TextField from 'material-ui/TextField';
+import LinearProgress from 'material-ui/LinearProgress';
+import RaisedButton from 'material-ui/RaisedButton';
+
+class PriceForm extends Component{
+
+  constructor(){
+    super();
+    this.calculateTax = this.calculateTax.bind(this);
+    this.state = {
+      totalTax: "0.00%",
+      costOfItem: "$0.00"
+    }
+  }
+
+  calculateTax(){
+    const itemCost = document.getElementById('itemCost').value;
+    const taxPercentage = document.getElementById('taxPercentage').value;
+    this.setState({
+      totalTax: itemCost,
+      costOfItem: taxPercentage
+    });
+  }
+
+  render(){
+    return(
+      <div className = "salesCard">
+        <Card>
+          <CardTitle title="Sales Tax Calculator" />
+          <CardText>
+          <form>
+            <TextField hintText="Item Cost" id="itemCost" value={this.state.totalTax} onChange={this.calculateTax}/>
+            <br />
+            <TextField hintText="Sales Tax Percentage (%)" id="taxPercentage" value={this.state.costOfItem} onChange={this.calculateTax}/>
+            <br />
+            <br />
+            <LinearProgress mode="determinate" />
+            <br />
+            <h1>{this.state.totalTax}</h1>
+            <br />
+            <h1>{this.state.costOfItem}</h1>
+            <br />
+          </form>
+          </CardText>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default PriceForm;
