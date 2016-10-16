80a922865b729ad0f97bf9e8455a071a32312097
Alex Knipfer <cr33pyxjtag@yahoo.com>
2016-10-16 16:50:47 -0500

    fixed if block for displaying new tax and price

--- src/PriceForm.js
+++ src/PriceForm.js
@@ -20,42 +20,48 @@ class PriceForm extends Component{
     const itemCost = document.getElementById('itemCost').value;
     const taxPercentage = document.getElementById('taxPercentage').value;
 
-    if(itemCost == ''){
+
+    if(itemCost=== '' && taxPercentage !== ''){
       this.setState({
-        costOfItem: "$0.00"
+        costOfItem: '$0.00',
+        totalTax: taxPercentage
       });
     }
 
-    else if(taxPercentage == ''){
+    else if(taxPercentage === '' && itemCost!== ''){
       this.setState({
-        totalTax: "0.00%"
+        costOfItem: itemCost,
+        totalTax: '0.00%'
       });
     }
 
-    else if(itemCost == '' && taxPercentage == ''){
+    else if(taxPercentage === '' && itemCost === ''){
       this.setState({
-        totalTax: "0.00%",
-        costOfItem: "$0.00"
+        costOfItem: '$0.00',
+        totalTax: '0.00%'
       });
     }
+
     else{
       this.setState({
         totalTax: itemCost,
         costOfItem: taxPercentage
       });
     }
+
+
   }
 
   render(){
     return(
-      <div className = "salesCard">
+      <div className="salesCard">
         <Card>
           <CardTitle title="Sales Tax Calculator" />
           <CardText>
           <form>
-            <TextField hintText="Item Cost" id="itemCost" value={this.state.totalTax} onChange={this.calculateTax}/>
+            <TextField hintText="Item Cost" id="itemCost" onChange={this.calculateTax}/>
             <br />
-            <TextField hintText="Sales Tax Percentage (%)" id="taxPercentage" value={this.state.costOfItem} onChange={this.calculateTax}/>
+            <TextField hintText="Sales Tax Percentage (%)" id="taxPercentage" onChange={this.calculateTax}/>
             <br />
             <br />
             <LinearProgress mode="determinate" />
