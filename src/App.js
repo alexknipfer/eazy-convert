f8b89bbd273bc346a1345be799b9700e32dc8b71
Alex Knipfer <cr33pyxjtag@yahoo.com>
2016-10-16 19:51:13 -0500

    project restructure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import PriceForm from './PriceForm.js';
+import PriceForm from './components/PriceForm/PriceForm.js';
 
 class App extends Component {
   render() {
